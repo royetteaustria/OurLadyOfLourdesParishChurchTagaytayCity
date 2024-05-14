d05fefc86f16cc2858b7ac437cfb74b9ff3ac256
royetteaustria <austriaroyette@gmail.com>
2024-05-14 13:49:07 +0800

    Configure the vite file on frontend

--- Frontend/vite.config.ts
+++ Frontend/vite.config.ts
@@ -8,7 +8,7 @@ export default defineConfig({
     port: 5100,
     proxy: {
       'api' : {
-        target:  'http://localhost:5000',
+        target:  'http://lour-lady-of-lourdes-parish-church-tagaytay-city.vercel.app',
         changeOrigin: true
       }
     }
