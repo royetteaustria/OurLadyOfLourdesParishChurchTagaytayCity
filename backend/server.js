329e42d6892d31c7e9787aff613a688788d12650
royetteaustria <austriaroyette@gmail.com>
2024-05-14 13:46:17 +0800

    Updating server js of cors into domain name of backend

--- backend/server.js
+++ backend/server.js
@@ -28,7 +28,7 @@ const app = express();
 dotenv.config();
 const PORT = process.env.PORT || 5000
 app.use(cors({ 
-    origin: 'http://localhost:5100', 
+    origin: 'http://our-lady-of-lourdes-parish-church-tagaytay-city.vercel.app', 
     credentials: true 
   }));
 
