4beeea25f8527dc125e71899889b5c6ff0cca4ef
royetteaustria <austriaroyette@gmail.com>
2024-05-14 14:58:14 +0800

    Updated server js

--- Frontend/vite.config.ts
+++ Frontend/vite.config.ts
@@ -1,19 +1,19 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
     port: 5100,
     proxy: {
-      '/api': {
-        target: 'https://our-lady-of-lourdes-parish-church-tagaytay-city.vercel.app',
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, '')
+      'api' : {
+        target:  'https://our-lady-of-lourdes-parish-church-tagaytay-city.vercel.app',
+        changeOrigin: true
       }
     }
   },
   optimizeDeps: {
     exclude: ["js-big-decimal"]
-  }
+  },
 })
