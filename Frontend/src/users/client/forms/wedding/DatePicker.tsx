ed454e884d2a14fbb2f5c1825387b8a22bf16915
royetteaustria <austriaroyette@gmail.com>
2024-05-15 12:36:57 +0800

    Update the URL path to domain name server in wedding reserve

--- Frontend/src/users/client/forms/wedding/DatePicker.tsx
+++ Frontend/src/users/client/forms/wedding/DatePicker.tsx
@@ -25,7 +25,7 @@ export function DatePicker({updateFields} : AvailableDateProps) {
   }, []);
 
   const fetchAvailableDates = () => {
-    axios.get<AvailableDate[]>("http://localhost:5000/api/CalendarReservation/available-dates")
+    axios.get<AvailableDate[]>("https://ourladyoflourdes-parishchurch-tagaytay-city-server.vercel.app/api/CalendarReservation/available-dates")
       .then(response => {
         const sortedDates = response.data.sort((a, b) => {
           return new Date(a.start).getTime() - new Date(b.start).getTime();
