70192df178b93600c33c1920899629e341c5e893
royetteaustria <austriaroyette@gmail.com>
2024-05-16 16:32:06 +0800

    Updated Report module on frontend

--- Frontend/src/users/weddingAdmin/pages/reports/Report.tsx
+++ Frontend/src/users/weddingAdmin/pages/reports/Report.tsx
@@ -214,10 +214,10 @@ const Report = () => {
                     </td>
       
                     <td className="border-b border-[#eee] py-5 px-4 dark:border-strokedark">
-                      <p className="font-normal ml-8 text-black dark:text-white">{report.Rites.toUpperCase()}</p>
+                      <p className="font-normal ml-8 text-black dark:text-white">{report.Rites.toUpperCase() === '' ? 'To be determined' : report.Rites.toUpperCase()}</p>
                     </td>
                     <td className="border-b border-[#eee] py-5 px-4 dark:border-strokedark">
-                      <p className="font-normal ml-6 text-black dark:text-white">{report.GuestPriest.toUpperCase()}</p>
+                      <p className="font-normal ml-6 text-black dark:text-white">{report.GuestPriest.toUpperCase() === '' ? 'To be determined' : report.GuestPriest.toUpperCase()}</p>
                     </td>
                     {/* <td className="border-b border-[#eee] py-5 px-4 dark:border-strokedark space-x-4">
                     <button className="text-warning">
