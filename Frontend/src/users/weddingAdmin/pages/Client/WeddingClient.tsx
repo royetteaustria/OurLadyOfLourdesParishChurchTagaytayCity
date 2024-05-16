29a7a3bb36db847dc9fbe7c33d030460cb174df8
royetteaustria <austriaroyette@gmail.com>
2024-05-16 16:44:33 +0800

    Updated Wedding client url localhost to domain name server

--- Frontend/src/users/weddingAdmin/pages/Client/WeddingClient.tsx
+++ Frontend/src/users/weddingAdmin/pages/Client/WeddingClient.tsx
@@ -38,7 +38,7 @@ const WeddingClient = () => {
     async function getInquiries() {
       try {
         const response = await axios.get(
-          `http://localhost:5000/api/WeddingClient/listOfClient`
+          `https://ourladyoflourdes-parishchurch-tagaytay-city-server.vercel.app/api/WeddingClient/listOfClient`
         );
         const inquiries = response.data;
         // Check if inquiries is an array before updating the state
