0574dba600529525b90b3bf711385911cf96d680
royetteaustria <austriaroyette@gmail.com>
2024-05-19 19:53:39 +0800

    Updated Wedding calendar schema

--- backend/model/manageReservation/CalendarReservation.js
+++ backend/model/manageReservation/CalendarReservation.js
@@ -3,15 +3,12 @@ import mongoose from "mongoose";
 const CalendarSchema = new mongoose.Schema({
     start: {
         type: Date,
-        unique: true,
         required: [true, "Please Insert The Start of your event"],
         min: [new Date(), "can't be before now!!"],
-        
         },
         
        end: {
         type: Date,
-        unique: true,
         min: [new Date(), "can't be before now!!"],
       },
     description: { 
@@ -20,6 +17,6 @@ const CalendarSchema = new mongoose.Schema({
     
 })
 
-const CalendarForReservation = mongoose.model('CalendarReservation', CalendarSchema)
+const CalendarForReservation = mongoose.model('WeddingCalendarReservation', CalendarSchema)
 
 export default CalendarForReservation
